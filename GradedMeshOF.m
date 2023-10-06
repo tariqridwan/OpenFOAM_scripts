2ab7367e0dddf378a3be4cf59186831c3a241317
Tariq Ridwan <105285635+tariqridwan@users.noreply.github.com>
2023-10-06 12:59:30 +0200

    Update GradedMeshOF.m

--- GradedMeshOF.m
+++ GradedMeshOF.m
@@ -1,22 +1,24 @@
-% This program calculates the smallest cell size: dx
-% for a Graded mesh created by blockMesh in OpenFOAM
+% This program calculates the smallest cell size: dx and maximum time-step size dt
+% for a Graded mesh created by blockMesh utility in OpenFOAM
 % Algorithm taken from OpenFOAM tutorial guide v2212, p. 34.
 %            -------
-% written by Tariq Ridwan: https://tariqridwan.github.io/
-% Universitat Politècnica de Catalunya
+% written by Tariq Ridwan: 👉 https://tariqridwan.github.io/
+% Universitat Politècnica de Catalunya, Barcelona
 clear
 format long
-l = 0.1/2;          % entire length if the domain's edge
+%% Domain/grid configuration
+l = 0.1/2;          % entire length of the domain's edge
 n = 40;             % number of cells
 Rr = 2;             % Ratio of length, Last/First cell
-r = Rr^(1/(n-1));   % Ratio between one cell and the next
+r = Rr^(1/(n-1));   % Ratio between one cell and the next // gradual stretching rate
 if Rr>1
     alpha = Rr;
 else
     alpha = 1-r^(-n)+1/r;
 end
-dx = l*(r-1)/(alpha*r-1)    % smallest cell size
-% Co = dt*U/dx                % Courant number
+%% CALCULATION
+dx = l*(r-1)/(alpha*r-1)    % Calculate smallest cell size
+% Co = dt*U/dx              % Courant number term
 Co = 1;
 U = 1;                      % velocity
-dt = Co*dx/U                % time-step size
+dt = Co*dx/U                % Calculate Maximum time-step size for the given courant number
